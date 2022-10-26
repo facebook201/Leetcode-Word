1f71f87432b59c8311a9cae0fa20b40caa7ab05f
syo <syolmm@qq.com>
2022-10-26 22:10:50 +0800

    位运算符

--- Operator/Operator.js
+++ Operator/Operator.js
@@ -9,7 +9,7 @@
  * 
  * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。 
  * 不使用额外的空间 
- * todo ^ 位运算 满足 0 ^ 0 = 0
+ * sy ^ 异或运算 满足相同数字 ^ 为零
  */
 const nums = [1, 2, 3, 1, 2, 3, 4, 7, 4];
 
@@ -17,20 +17,11 @@ function singleNumber(nums) {
   if (!nums || !nums.length) return;
 
   let result = 0;
+  // 选择
   for (const num of nums) {
     result ^= num; 
   }
-
   console.log(result);
 }
 
 singleNumber(nums);
-
-
-
-
-
-
-
-
-
